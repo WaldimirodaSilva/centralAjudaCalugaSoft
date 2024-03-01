cf89ebacceec8aa584ebfc273b0da7865dded4ea
Valdemiro Salvador <valdemirocamikase55@gmail.com>
2024-03-01 15:12:05 +0000

    atualização do arquivo: paginasPrincipais
    implementado a função de visualização de artigos e seus passos.Obs: ainda falta terminar a parte de visualização dos passos

--- app/controller/paginaPrincipais.php
+++ app/controller/paginaPrincipais.php
@@ -1,8 +1,9 @@
 <?php
-	//ola mundo
 	// Esta classe trabalha entregando que dados sera móstrada ao usúario
-	class paginaPrincipais{
-		public function home(){
+	class paginaPrincipais
+	{
+		public function home()
+	{
 			try{
 				// pegando os dados no banco de dados
 				$colecSoftware = dadosDatabase::softwares();
@@ -18,6 +19,12 @@
 				// renderizando a página para o usúario
 				$conteudo = $template->render($parametros);
 
+				if (isset($_SESSION['softwareInserido'])) {
+					helper::mensagem('softwareInserido');
+				}elseif (isset($_SESSION['softwareErro'])) {
+					helper::mensagem('softwareErro');
+				}
+
 				echo $conteudo;
 
 			}catch(PDOException $e){
@@ -27,8 +34,10 @@
 
 		// os metodos abaixo seguem o mesmo conceito do primeiro
 
-		public function artigo($params){
-			try{ 
+		public function artigo($params)
+		{
+			try
+			{ 
 				$colecArtigo = dadosDatabase::artigos($params[0]);
 
 				$loader = new \Twig\Loader\FilesystemLoader('app/veiw');
@@ -38,8 +47,31 @@
 				$parametros = array();
 				$parametros['id'] = $params[0];
 				$parametros['artigos'] = $colecArtigo;
+				$parametros['vetor'] = array(10,45,33);
+
+				// pegando id de cada artigo para chamar os seus passos
+				if ($colecArtigo != null) {
+					foreach ($colecArtigo as $artigo) {
+						// pegando os passos do banco de dados desse artigo
+						$passosArtigo = dadosDatabase::passos($artigo->id);
+					
+						$parametros['artigo'.$artigo->id] = $passosArtigo; 
+					}
+				}
 
 				$conteudo = $template->render($parametros);
+
+				if (isset($_SESSION['artigoInserido'])) {
+					helper::mensagem('artigoInserido');
+				}elseif (isset($_SESSION['artigoErro'])) {
+					helper::mensagem('artigoErro');
+				}
+
+				if (isset($_SESSION['passoInserido'])) {
+					helper::mensagem('passoInserido');
+				}elseif (isset($_SESSION['passoErro'])) {
+					helper::mensagem('passoErro');
+				}
 
 				echo $conteudo;
 			}catch(PDOException $e){
@@ -47,15 +79,18 @@
 			}
 		} 
  
-		public function editarArtigo(){
+		public function editarArtigo()
+		{
 			echo file_get_contents('app/veiw/editarArtigo.html');
 		}
 
-		public function erroPedido(){ 
+		public function erroPedido()
+		{ 
 			echo "erro pagina não existente";
 		}
 
-		public function perguntasFrequentes(){
+		public function perguntasFrequentes()
+		{
 			echo file_get_contents('app/veiw/perguntasFrequentes.html');	
 		}
 	}
